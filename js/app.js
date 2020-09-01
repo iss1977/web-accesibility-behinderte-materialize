efd41a0ca5b3c93d550fb2a12f2c2527313aa38c
Ioan-Sebastian Szücs <ioan.szucs@gmail.com>
2020-09-01 15:16:18 +0200

    End of day ... some divs exists ...

---
+++ js/app.js
@@ -0,0 +1,9 @@
+/*This is my app.js*/
+
+var elem = document.querySelector('.tabs');
+     var options = {};
+     var instance = M.Tabs.init(elem, options);
+
+var instance = M.Tabs.init(elem, options);
+
+console.log(instance);
